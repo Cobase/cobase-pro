52a9f85abfcf966da7f1b79fd41c2609bd691c0d
Johan Ruokangas <johan@latehours.net>
2016-02-24 22:25:14 +0200

    Split Sidebar to multiple components

--- client/src/components/LoggedIn.js
+++ client/src/components/LoggedIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Sidebar from './Sidebar';
+import Sidebar from './sidebar/Sidebar';
 
 export default class LoggedIn extends Component {
   render() {
---
+++ client/src/components/sidebar/SidebarSectionTitle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SidebarSectionTitle = ({children}) => (
+  <h3 className="sidebar-navigation-title">{children}</h3>
+);
+
+export default SidebarSectionTitle;
